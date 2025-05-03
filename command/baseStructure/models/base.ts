99f844e51c7bf9bf09b0fcb02c44d1c1dfe356d1
Debasish <debasish@ascentspark.com>
2025-05-03 12:49:57 +0530

    Fixed

--- command/baseStructure/models/base.ts
+++ command/baseStructure/models/base.ts
@@ -21,6 +21,6 @@ export const schema = {
     // blobField: { type: 'BLOB', allowNull: true }      
 };
 
-export const tableName = 'MBases';        // optional
+export const tableName = 'SBases';        // optional
 export const timestamps = true;          // optional
 export const softDelete = true;          // optional
