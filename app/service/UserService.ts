7fcfedee7556f1bd02faedac58a5e082f8825f6e
Debasish <debasish@ascentspark.com>
2025-05-03 07:37:22 +0530

    Fixed Structure

--- app/service/UserService.ts
+++ app/service/UserService.ts
@@ -1,8 +1,8 @@
 
 import { DatabaseService } from '@system/databaseService';
-import { BaseService } from './BaseService';
+import { ModelConnection } from '@utils/ModelConnection';
 
-export class UserService extends BaseService{
+export class UserService extends ModelConnection{
   constructor() {
     super();
   }
@@ -11,7 +11,7 @@ export class UserService extends BaseService{
       if (!userData.name || !userData.email) {
         throw new Error('Name and email are required');
       }
-      const {User,softDelete} = await BaseService.getUserModel();
+      const {User,softDelete} = await ModelConnection.getModel('User');
       return await DatabaseService.create(User, userData, softDelete);
     } catch (error: any) {
       throw new Error(`Error creating user: ${error.message}`);
@@ -20,7 +20,7 @@ export class UserService extends BaseService{
 
   static async getAll() {
     try {
-      const {User,softDelete} = await BaseService.getUserModel();
+      const {User,softDelete} = await ModelConnection.getModel('User');
       return await DatabaseService.get(User,{}, softDelete);
     } catch (error: any) {
       throw new Error(`Error fetching users: ${error.message}`);
@@ -29,7 +29,7 @@ export class UserService extends BaseService{
 
   static async getById(id: string) {
     try {
-      const {User,softDelete} = await BaseService.getUserModel();
+      const {User,softDelete} = await ModelConnection.getModel('User');
       return await DatabaseService.getById(User, id, softDelete);
     } catch (error: any) {
       throw new Error(`Error fetching user by ID: ${error.message}`);
@@ -38,7 +38,7 @@ export class UserService extends BaseService{
 
   static async update(id: string | number, userData: any) {
     try {
-      const {User,softDelete} = await BaseService.getUserModel();
+      const {User,softDelete} = await ModelConnection.getModel('User');
       return await DatabaseService.update(User, id, userData);
     } catch (error: any) {
       throw new Error(`Error updating user: ${error.message}`);
@@ -47,7 +47,7 @@ export class UserService extends BaseService{
 
   static async delete(id: string | number) {
     try {
-      const {User,softDelete} = await BaseService.getUserModel();
+      const {User,softDelete} = await ModelConnection.getModel('User');
       return await DatabaseService.delete(User, id);
     } catch (error: any) {
       throw new Error(`Error deleting user: ${error.message}`);
--- app/utils/ModelConnection.ts
+++ app/utils/ModelConnection.ts
@@ -2,14 +2,14 @@ import { getSQLModels } from '@system/sqlAdapter'; // dynamic SQL loader
 import { getMongoModels } from '@system/mongoAdapter'; // dynamic Mongo loader
 import { activeDbType } from '@system/types';
 
-export class BaseService {
-  protected static async getUserModel(): Promise<any> {
+export class ModelConnection {
+  protected static async getModel(Model:any): Promise<any> {
     if (activeDbType === 'mongo') {
       const cachedMongoModels: any = await getMongoModels();
-      return { User: cachedMongoModels.User, softDelete: cachedMongoModels.User.schema.options.softDelete };
+      return { [Model]: cachedMongoModels[Model], softDelete: cachedMongoModels[Model].schema.options.softDelete };
     } else {
       const cachedModels = await getSQLModels(activeDbType);
-      return { User: cachedModels.User, softDelete: cachedModels.User.schemaOptions?.softDelete };
+      return { [Model]: cachedModels[Model], softDelete: cachedModels[Model].schemaOptions?.softDelete };
     }
   }
 }
